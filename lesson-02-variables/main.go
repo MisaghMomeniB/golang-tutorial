4166ed81a2bc63367f9707668772c1d00b187c34
MisaghMomeniB <jenabemisagh@gmail.com>
2026-06-07 11:01:37 +0330

    define variables, print to console

---
+++ lesson-02-variables/main.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	var name string = "YourName"
+
+	age := 100000
+
+	var height int = 100000
+
+	var weight float64 = 100000.5
+
+	isStudent := true
+
+	fmt.Println("Name:", name)
+	fmt.Println("Age:", age)
+	fmt.Println("Height:", height)
+	fmt.Println("Weight:", weight)
+	fmt.Println("Is Student:", isStudent)
+}
