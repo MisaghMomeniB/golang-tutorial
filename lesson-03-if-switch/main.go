c31f1e7a6d0a4929fd4c334f3d6dab283644de5d
MisaghMomeniB <jenabemisagh@gmail.com>
2026-06-14 13:57:31 +0330

    if/else if/ else / OR

--- lesson-03-if-switch/main.go
+++ lesson-03-if-switch/main.go
@@ -34,11 +34,20 @@ import "fmt"
 // 	}
 // }
 
-func main() {
-	age := 25
-	hasLicense := true
+// func main() {
+// 	age := 25
+// 	hasLicense := true
 
-	if age >= 18 && hasLicense {
-		fmt.Println("Can Drive")
+// 	if age >= 18 && hasLicense {
+// 		fmt.Println("Can Drive")
+// 	}
+// }
+
+func main() {
+	isAdmin := false
+	isOwner := true
+
+	if isAdmin || isOwner {
+		fmt.Println("Access Granted")
 	}
 }
