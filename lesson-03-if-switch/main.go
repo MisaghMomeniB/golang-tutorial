dcea034165304aa31ed910633c5064fd956fc53c
MisaghMomeniB <jenabemisagh@gmail.com>
2026-06-14 13:55:51 +0330

    if/else

--- lesson-03-if-switch/main.go
+++ lesson-03-if-switch/main.go
@@ -2,10 +2,20 @@ package main
 
 import "fmt"
 
+// func main() {
+// 	age := 20
+
+// 	if age >= 18 {
+// 		fmt.Println("Adult")
+// 	}
+// }
+
 func main() {
-	age := 20
+	age := 16
 
 	if age >= 18 {
 		fmt.Println("Adult")
+	} else {
+		fmt.Println("Under Age")
 	}
 }
